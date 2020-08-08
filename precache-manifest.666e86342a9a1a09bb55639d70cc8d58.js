10c8c58b8fafeb083aaf7a2d465458c48ef00dbd
Faisal Malik <faisalmalik@Faisals-MacBook-Pro.local>
2020-08-08 14:40:41 -0400

    Updates

---
+++ precache-manifest.666e86342a9a1a09bb55639d70cc8d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92868e7b0e0734075779d3eab8286fc",
+    "url": "/frequentbrowsers/index.html"
+  },
+  {
+    "revision": "58b32fc639edd3401829",
+    "url": "/frequentbrowsers/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "deb5b462e1f162373a33",
+    "url": "/frequentbrowsers/static/css/main.ce60d0aa.chunk.css"
+  },
+  {
+    "revision": "58b32fc639edd3401829",
+    "url": "/frequentbrowsers/static/js/2.01d72e80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frequentbrowsers/static/js/2.01d72e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb5b462e1f162373a33",
+    "url": "/frequentbrowsers/static/js/main.df634ec6.chunk.js"
+  },
+  {
+    "revision": "25008ecc5a62449b70d2",
+    "url": "/frequentbrowsers/static/js/runtime-main.a02e1c5c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frequentBrowsers/precache-manifest.1bc31ed964ac67895794cb5653875d58.js"
+  "/frequentbrowsers/precache-manifest.666e86342a9a1a09bb55639d70cc8d58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frequentBrowsers/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frequentbrowsers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
